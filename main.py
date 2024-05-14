d3414ba43f32cdfc510283fd5376fa536aac57db
TrianDii <134402856+CookieTaberu@users.noreply.github.com>
2024-05-14 09:31:05 +0700

    early acces v0.1

--- main.py
+++ main.py
@@ -4,25 +4,16 @@ from dataproses import preproses
 import InputProcess
 from InputProcess import predict
 
-def main():
+def main(userInputData):
     # Load your data (replace 'your_data.csv' with your actual data file)
     data = pd.read_csv('loan_data.csv')
     
     # Train the model
     train_model(data)
 
-    # Example of predicting with new input data
-    new_input_data = { 'int.rate': 0.1,
-        'installment': 500,
-        'dti': 0.3,
-        'fico': 700,
-        'days.with.cr.line': 2000,
-        'revol.bal': 200,
-        'revol.util': 40,
-        'delinq.2yrs': 0,
-        'inq.last.6mths': 1}  # Replace {...} with your new input data
-    user=InputProcess.ProcessingInput(new_input_data)
-    predict(user)
+    user=InputProcess.ProcessingInput(userInputData)
+    a = predict(user)
+    return a
     
 
 if __name__ == "__main__":
