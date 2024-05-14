f598a4ffcf51f3c6c51fb514bf65a8d309f9eddb
TrianDii <134402856+CookieTaberu@users.noreply.github.com>
2024-05-14 08:15:40 +0700

    Fix all

--- InputProcess.py
+++ InputProcess.py
@@ -4,26 +4,15 @@ import dataproses
 from dataproses import preproses
 import pickle
 def ProcessingInput(newInput):
-    clf = DecisionTreeClassifier()
-    X_train,y_train = preproses(newInput)
-    clf.fit(X_train,y_train)
     # Create a DataFrame from the new applicant's features
     new_applicant_df = pd.DataFrame([newInput])
-
-    # Reorder columns to match the training data
-    new_applicant_df = new_applicant_df[X_train.columns]
-
-    # Predict loan repayment for the new applicant
-    prediction = clf.predict(new_applicant_df)
     return new_applicant_df
 
 def predict(newInput):
     with open('tree_model.pkl', 'rb') as file:
         loaded_model = pickle.load(file)
-        
-    prepas = ProcessingInput(newInput)
     
-    predict = loaded_model.predict(prepas)
+    predict = loaded_model.predict(newInput)
 
     if predict[0] == 0:
         print("Prediction: Fully Paid")
